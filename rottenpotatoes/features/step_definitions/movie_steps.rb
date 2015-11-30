cd4a725195c1f4a2c36eef76f3b6e481e3e35ac2
hungtn95 <hungtn95@gmail.com>
2015-11-30 13:35:19 +0000

    Cucumber full BDD

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -3,9 +3,9 @@
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    # you should arrange to add that movie to the database here
+    Movie.create!(movie)
   end
-  fail "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +14,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  expect(page.body).to have_content(/.*#{e1}.*#{e2}/m)
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,10 +25,19 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  if uncheck == "un" then
+    for rating in rating_list.split(", ")
+      uncheck("ratings[#{rating}]")
+    end
+  end
+  if uncheck == nil then
+    for rating in rating_list.split(", ")
+      check("ratings[#{rating}]")
+    end
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  page.all("#movies tr").count.should == Movie.count + 1
 end
